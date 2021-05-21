12f9b7d88715ea95595b185784645768d844ceb0
pierrestephanekanga <pierreatephanekanga@gmail.com>
2021-05-21 18:20:52 +0000

    session spark

--- src/main/scala/SparkApp.scala
+++ src/main/scala/SparkApp.scala
@@ -18,7 +18,22 @@ object SparkApp {
 
     val rdd1 = ss.sparkContext.parallelize(Seq("Ma premiere application spark. Je suis un etudiant de DATA SCIENCE heureux"))
 
-    rdd1.foreach(l => println(l))
+    //rdd1.foreach(l => println(l))
+
+    val df_1 = ss.read
+      .format("csv")
+      .option("InferSchema", "True") //permet de faire l'inference (type des collonne)
+      .option("header", "True")
+      .option("delimiter", ";")
+      .csv(path = "D:\\MASTER_1_DATA_SCIENCE\\DEUXIEME_SEMESTRE_2020-2021\\HADOOP_Prof_JUVENAL\\orders_csv.csv")
+
+    df_1.show(numRows = 10)
+
+    df_1.printSchema()
+
+    df_1.createOrReplaceTempView(viewName = "orders") // creation du schema de la
+
+    ss.sql(sqlText = "select * from orders where city = 'NEWTON' ").show() // execution dune requete sql
 
 
   }
